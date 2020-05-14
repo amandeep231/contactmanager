b6c26420af20cf0d4fd1b4afc2a6615aa6d3696f
amandeep231 <singhlove3435@gmail.com>
2020-05-14 17:17:05 +0530

    initial commit

---
+++ build/precache-manifest.8018e00a6ea43db8791deb18073be01c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4dd316244f4b9d1062815e596fcd8e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b00aba21c0aa5879bf03",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ef9136ecace41068434e",
+    "url": "/static/js/2.66b4b22f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.66b4b22f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b00aba21c0aa5879bf03",
+    "url": "/static/js/main.fc7b57d2.chunk.js"
+  },
+  {
+    "revision": "a3a2677fbe4acf1a0207",
+    "url": "/static/js/runtime-main.a6974fca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
